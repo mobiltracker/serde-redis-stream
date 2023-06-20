ad7d1c97ee3f939b1b83217ae26c330084208b66
Tiago Schmidt <schmidttiago7@gmail.com>
2023-06-20 08:04:40 -0300

    Finishing mirror implementation for this error.

--- serde-redis-stream-interface/src/lib.rs
+++ serde-redis-stream-interface/src/lib.rs
@@ -1,10 +1,14 @@
-use redis::{streams::StreamKey, FromRedisValue};
+use redis::streams::StreamKey;
 use thiserror::Error;
 
 #[derive(Debug, Error)]
 pub enum RedisStreamDeriveError {
-    #[error("Field in hash maps wasn't found: `{0}`")]
+    #[error("Missing field in StreamKey(HashMap): `{0}`")]
     MissingFieldFromHashMap(String),
+    #[error("Error in deserialization from redis value in: `{0}`")]
+    DeserializationErrorFromRedisValue(String),
+    #[error("Item on StreamKey is invalid.")]
+    InvalidItemOnStreamKey,
 }
 
 #[derive(Debug)]
@@ -35,20 +39,22 @@ impl RedisStreamSerializable for Foobar {
     fn redis_deserialize(value: StreamKey) -> Result<Self, RedisStreamDeriveError> {
         let ids = value.ids;
 
-        let map = &ids.first().unwrap().map;
+        let map = &ids
+            .first()
+            .ok_or(RedisStreamDeriveError::InvalidItemOnStreamKey)?
+            .map;
 
         let name: &redis::Value = map
             .get("name")
             .ok_or_else(|| RedisStreamDeriveError::MissingFieldFromHashMap(String::from("name")))?;
-        let name: String =
-            <String as redis::FromRedisValue>::from_redis_value(name).expect("TODO DESERIALIZE");
+        let name: String = <String as redis::FromRedisValue>::from_redis_value(name)
+            .map_err(|_| RedisStreamDeriveError::MissingFieldFromHashMap(String::from("name")))?;
 
         let age = map
             .get("age")
-            .map(i64::from_redis_value)
-            .transpose()
-            .expect("TODO - DESERIALIZATION")
-            .expect("TODO - NO STRING");
+            .ok_or_else(|| RedisStreamDeriveError::MissingFieldFromHashMap(String::from("age")))?;
+        let age: i64 = <i64 as redis::FromRedisValue>::from_redis_value(age)
+            .map_err(|_| RedisStreamDeriveError::MissingFieldFromHashMap(String::from("age")))?;
 
         Ok(Foobar { name, age })
     }
